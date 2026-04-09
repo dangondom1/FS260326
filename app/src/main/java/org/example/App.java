91884c6278039cc53c50973db4b3e6e10838356c
Daniel Gonzalez <daniglezdom@gmail.com>
2026-04-09 10:37:47 +0200

    Cambio App

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -5,7 +5,7 @@ package org.example;
 
 public class App {
     public String getGreeting() {
-        return "Hola mundo me llamo <uvus>";
+        return "Hola mundo me llamo dangondom1";
     }
 
     public static void main(String[] args) {
